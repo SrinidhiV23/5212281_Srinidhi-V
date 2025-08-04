3f0850405f2ffee7c91c084b2a35dc3c5db63acf
SrinidhiV23 <srinidhi2523@gmail.com>
2025-08-04 17:02:52 +0530

    Added Switch Homework Code

---
+++ C_Programming/Switch_HW.c
@@ -0,0 +1,29 @@
+//print the days and to repeat the input prompt only when the entered day is invalid 
+#include<stdio.h>
+int main(){
+    int day;
+    do{
+    printf("Enter a days btw 0 to 7:");
+    scanf("%d",&day);
+   
+    switch(day){
+        case 1: printf("Monday\n");
+               break;
+        case 2: printf("Tuesday\n");
+               break;
+        case 3: printf("Wednesday\n");
+               break;
+        case 4: printf("Thursday\n");
+               break;
+        case 5: printf("Friday\n");
+               break;
+        case 6: printf("Saturday\n");
+               break;
+        case 7: printf("sunday\n");
+               break;
+        default: printf("Invalid . Enter correct choice\n");
+    }              
+
+    }while(day < 1 || day > 7);
+    return 0;
+}
