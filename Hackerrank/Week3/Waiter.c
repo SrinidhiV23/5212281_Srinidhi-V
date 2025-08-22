f1e72d95fb59d92c867a545c5b6268ed4af91d3c
SrinidhiV23 <srinidhi2523@gmail.com>
2025-08-22 15:43:12 +0530

    Added Waiter codes

---
+++ Hackerrank/Week3/Waiter.c
@@ -0,0 +1,271 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'waiter' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY number
+ *  2. INTEGER q
+ */
+
+/*
+ * To return the integer array from the function, you should:
+ *     - Store the size of the array to be returned in the result_count variable
+ *     - Allocate the array statically or dynamically
+ *
+ * For example,
+ * int* return_integer_array_using_static_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     static int a[5] = {1, 2, 3, 4, 5};
+ *
+ *     return a;
+ * }
+ *
+ * int* return_integer_array_using_dynamic_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     int *a = malloc(5 * sizeof(int));
+ *
+ *     for (int i = 0; i < 5; i++) {
+ *         *(a + i) = i + 1;
+ *     }
+ *
+ *     return a;
+ * }
+ *
+ */
+ int isprime(int n){
+    if(n<2) return 0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+ }
+ void generateprime(int* prime, int q){
+    int num=2, count=0;
+    while (count<q) {
+        if(isprime(num)){
+            prime[count++]=num;
+        }
+        num++;
+    
+    }
+ }
+int* waiter(int number_count, int* number, int q, int* result_count) {
+    int* prime= malloc(q * sizeof(int));
+    generateprime(prime, q);
+    
+    int* ans= malloc(number_count * sizeof(int));
+    *result_count=0;
+    
+    int* A=malloc(number_count * sizeof(int));
+    int a_count= number_count;
+    for(int i=0;i<number_count;i++){
+        A[i] = number[i];
+    }
+    
+    int* B = malloc(number_count * sizeof(int));
+    for(int i=0;i<q;i++){
+        int b_count=0;
+        int new_count=0;
+        int* newA= malloc(number_count*sizeof(int));
+        
+        for(int j=a_count-1;j>=0;j--){
+            if(A[j]%prime[i]==0){
+                B[b_count++]=A[j];
+            }else{
+                newA[new_count++]=A[j];
+            
+            }
+        }
+        for(int j=b_count-1;j>=0;j--){
+            ans[(*result_count)++]=B[j];
+        }
+        a_count=new_count;
+        for(int j=0;j<new_count;j++){
+            A[j]=newA[j];
+        }
+        free(newA);
+    }
+    for(int j=a_count-1;j>=0;j--){
+        ans[(*result_count)++]=A[j];
+    }
+    free(A);
+    free(B);
+    free(prime);
+    return ans;
+}
+int main(){
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char** first_multiple_input = split_string(rtrim(readline()));
+
+    int n = parse_int(*(first_multiple_input + 0));
+
+    int q = parse_int(*(first_multiple_input + 1));
+
+    char** number_temp = split_string(rtrim(readline()));
+
+    int* number = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        int number_item = parse_int(*(number_temp + i));
+
+        *(number + i) = number_item;
+    }
+
+    int result_count;
+    int* result = waiter(n, number, q, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%d", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
